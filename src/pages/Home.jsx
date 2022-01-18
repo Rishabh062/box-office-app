9927c918a5ca016e3cde0fcb57305e761853b153
Rishabh Dwivedi <rishabhdwivedi062@gmail.com>
2022-01-19 01:30:49 +0530

    Added api fetching

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout';
+import {apiGet} from '../misc/config.jsx'
 
 const Home = () => {
+
+    const [input, setInput] = useState('');
+
+    const [results,setResults] = useState(null);    
+    const onSearch = () => {
+       
+        apiGet(`/search/shows?q=${input}`).then(result=>{
+            setResults(result)    
+            console.log(result);
+            });
+       
+    };
+
+    const onInputChange = (e) => {
+        setInput(e.target.value);
+      
+    }
+
+    const onKeyDown = (e)=>{
+        onSearch()
+    }
+    const renderResults = () => {
+        if(results && results.length === 0){
+            return <div>No results</div>
+        }
+        if(results && results.length > 0){
+            return <div> {results.map((item)=><div key={item.show.id}>{item.show.name}</div>)}</div>
+        }
+        return null;
+    }
     return (
-        <MainPageLayout>This is my home</MainPageLayout>
+        <MainPageLayout>
+            <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
+            <button type='button' onClick={onSearch}>Search</button>
+            {renderResults()}
+        </MainPageLayout>
     )
 };
 
