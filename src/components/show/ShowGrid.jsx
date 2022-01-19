e5ea22e083cc0eedadf3e29729ddafeaadda4386
Rishabh Dwivedi <rishabhdwivedi062@gmail.com>
2022-01-19 23:09:28 +0530

    added cards

---
+++ src/components/show/ShowGrid.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ShowCard from './ShowCard'
+import IMAGE_NOT_FOUND from '../../images/logo192.png'
+
+const ShowGrid = ( { data }) => {
+    return (
+        <div>
+            {
+             data.map( ({show})=>(<ShowCard key={show.id} id={show.id} name={show.name} image={show.image ? show.image.medium: IMAGE_NOT_FOUND} summary={show.summary}/>))
+            }
+        </div>
+    )
+}
+
+export default ShowGrid
