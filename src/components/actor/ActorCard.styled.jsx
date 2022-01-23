38b838a107bfd423b6ebce9591a163cfe72e01da
Rishabh Dwivedi <rishabhdwivedi062@gmail.com>
2022-01-23 22:49:30 +0530

    Added style

---
+++ src/components/actor/ActorCard.styled.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { SearchCard } from "../styled";
+
+export const StyledActorCard = styled(SearchCard)`
+
+  .deathday{
+      margin-top: 15px;
+      font-size: 14px;
+      font-weight: bold;
+  }
+
+`
