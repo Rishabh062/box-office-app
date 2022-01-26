1d323f25367f41d286a5dd986b42c31c7034dcea
Rishabh Dwivedi <rishabhdwivedi062@gmail.com>
2022-01-26 16:37:28 +0530

    Added show page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from 'react'; 
 import {Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.jsx';
+import Show from './pages/Show.jsx';
 import Starred from './pages/Starred.jsx';
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
     <Routes>
       <Route index element={<Home/> }/> 
       <Route exact path="starred" element={<Starred/>}/> 
-      <Route/>
+      <Route exact path="/show/:id" element={<Show/>}/>
+      <Route element="Not found"/>
     </Routes>
   );
 }
