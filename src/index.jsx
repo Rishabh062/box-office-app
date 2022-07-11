dee1dba32e2d190e26c86083005199010764a538
Rishabh Dwivedi <rishabhdwivedi062@gmail.com>
2022-07-11 13:05:53 +0530

    Removed github pages

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter} from 'react-router-dom';
 import App from './App.jsx';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <BrowserRouter>
@@ -13,7 +13,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
